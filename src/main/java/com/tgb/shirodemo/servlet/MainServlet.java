824228c64275229432ff07190f8a497390c98c38
liyuexin <1290821453@qq.com>
2017-11-06 10:29:00 +0800

    update

--- src/main/java/com/tgb/shirodemo/servlet/MainServlet.java
+++ src/main/java/com/tgb/shirodemo/servlet/MainServlet.java
@@ -6,15 +6,21 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
+@WebServlet(urlPatterns = {"/main"})
 public class MainServlet extends HttpServlet {
 
     private static final long serialVersionUID = 11L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
